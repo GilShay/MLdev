8c3f6b2b23e643bcab2be9d8884622656fedfe47
unknown <gilshayu@gmail.com>
2018-02-16 21:52:45 +0200

    update

--- My/ML2/Part2/MNIST.py
+++ My/ML2/Part2/MNIST.py
@@ -2,6 +2,7 @@ from sklearn.datasets import fetch_mldata
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
+from sklearn.linear_model import SGDClassifier
 
 mnist = fetch_mldata('MNIST original')
 print mnist
@@ -24,3 +25,13 @@ print X[36000]
 X_train, X_test, y_train, y_test = X[:60000], X[60000:], y[:60000], y[60000:]
 shuffle_index = np.random.permutation(60000)
 X_train, y_train = X_train[shuffle_index], y_train[shuffle_index]
+
+y_train_5 = (y_train == 5)
+y_test_5 = (y_test == 5)
+
+
+sgd_clf = SGDClassifier(random_state=42)
+sgd_clf.fit(X_train, y_train_5)
+
+print sgd_clf
+print sgd_clf.predict([some_digit])
