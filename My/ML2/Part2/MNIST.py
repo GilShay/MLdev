fba2ebace97ba2a006c35eb281e44a26354ee430
Gil Shaul-Shay <gilshayu@gmail.com>
2018-02-19 17:47:30 +0200

    cross validation

--- My/ML2/Part2/MNIST.py
+++ My/ML2/Part2/MNIST.py
@@ -35,3 +35,25 @@ sgd_clf.fit(X_train, y_train_5)
 
 print sgd_clf
 print sgd_clf.predict([some_digit])
+
+
+from sklearn.model_selection import cross_val_score
+print cross_val_score(sgd_clf, X_train, y_train_5, cv=3, scoring="accuracy")
+
+from sklearn.base import BaseEstimator
+class Never5Classifier(BaseEstimator):
+    def fit(self, X, y=None):
+        pass
+    def predict(self, X):
+        return np.zeros((len(X), 1), dtype=bool)
+
+never_5_clf = Never5Classifier()
+print cross_val_score(never_5_clf, X_train, y_train_5, cv=3, scoring='accuracy')
+
+from sklearn.model_selection import cross_val_predict
+y_train_pred = cross_val_predict(sgd_clf,X_train, y_train_5, cv=3)
+
+from sklearn.metrics import confusion_matrix
+print confusion_matrix(y_train_5, y_train_pred)
+
+####Precision and Recall
