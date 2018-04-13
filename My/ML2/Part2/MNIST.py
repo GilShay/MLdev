e0a3a57255a15f75103c4bc092efd1af8a130bde
GilShay <gilshayu@gmail.com>
2018-04-13 21:17:28 +0300

    GUI + updated

--- My/ML2/Part2/MNIST.py
+++ My/ML2/Part2/MNIST.py
@@ -182,3 +182,56 @@ X_train_scaled = scaler.fit_transform(X_train.astype(np.float64))
 print cross_val_score(sgd_clf, X_train_scaled, y_train, cv=3, scoring="accuracy")
 
 
+
+###Error Analysis
+y_train_pred = cross_val_predict(sgd_clf, X_train_scaled, y_train, cv=3)
+conf_mx = confusion_matrix(y_train, y_train_pred)
+print conf_mx
+plt.matshow(conf_mx, cmap=plt.cm.gray)
+plt.show()
+
+row_sums = conf_mx.sum(axis=1, keepdims=True)
+print row_sums
+conf_mx = np.array(conf_mx, dtype=float)
+print conf_mx
+norm_conf_mx = conf_mx/ row_sums
+print norm_conf_mx
+
+np.fill_diagonal(norm_conf_mx, 0)
+print norm_conf_mx
+plt.matshow(norm_conf_mx, cmap=plt.cm.gray)
+plt.show()
+
+cl_a, cl_b =3, 5
+X_aa = X_train[(y_train == cl_a) & (y_train_pred == cl_a)]
+X_ab = X_train[(y_train == cl_a) & (y_train_pred == cl_b)]
+X_ba = X_train[(y_train == cl_b) & (y_train_pred == cl_a)]
+X_bb = X_train[(y_train == cl_b) & (y_train_pred == cl_b)]
+
+def plot_digit(data):
+    image = data.reshape(28, 28)
+    plt.imshow(image, cmap = matplotlib.cm.binary,
+               interpolation="nearest")
+    plt.axis("off")
+
+def plot_digits(instances, images_per_row=10, **options):
+    size = 28
+    images_per_row = min(len(instances), images_per_row)
+    images = [instance.reshape(size,size) for instance in instances]
+    n_rows = (len(instances) - 1) // images_per_row + 1
+    row_images = []
+    n_empty = n_rows * images_per_row - len(instances)
+    images.append(np.zeros((size, size * n_empty)))
+    for row in range(n_rows):
+        rimages = images[row * images_per_row : (row + 1) * images_per_row]
+        row_images.append(np.concatenate(rimages, axis=1))
+    image = np.concatenate(row_images, axis=0)
+    plt.imshow(image, cmap = matplotlib.cm.binary, **options)
+    plt.axis("off")
+
+plt.figure(figsize=(8,8))
+plt.subplot(221); plot_digits(X_aa[:25], images_per_row=5)
+plt.subplot(222); plot_digits(X_ab[:25], images_per_row=5)
+plt.subplot(223); plot_digits(X_ba[:25], images_per_row=5)
+plt.subplot(224); plot_digits(X_bb[:25], images_per_row=5)
+plt.show()
