e96018743f96c3e0818d70b7b988fa2c6fd34853
GilShay <gilshayu@gmail.com>
2018-04-29 22:27:42 +0300

    GridSearchCV

--- My/DataSchool/CrossValidation/KFoldCrossValidation.py
+++ My/DataSchool/CrossValidation/KFoldCrossValidation.py
@@ -16,18 +16,33 @@ print scores
 print scores.mean()
 k_range = range(1,31)
 k_scores = []
-# for k in k_range:
-#     knn = KNeighborsClassifier(n_neighbors=k)
-#     scores = cross_val_score(knn, X, y, cv=10, scoring='accuracy')
-#     k_scores.append(scores.mean())
-# print k_scores
-# plt.plot(k_range, k_scores)
-# plt.xlabel('Value of K for KNN')
-# plt.ylabel('Cross valudation accuracy')
-# plt.show()
+for k in k_range:
+     knn = KNeighborsClassifier(n_neighbors=k)
+     scores = cross_val_score(knn, X, y, cv=10, scoring='accuracy')
+     k_scores.append(scores.mean())
+print k_scores
+plt.plot(k_range, k_scores)
+plt.xlabel('Value of K for KNN')
+plt.ylabel('Cross valudation accuracy')
+plt.show()
 
 knn = KNeighborsClassifier(n_neighbors=20)
 print cross_val_score(knn, X, y, cv=10, scoring="accuracy").mean()
 
 logreg = LogisticRegression()
 print cross_val_score(logreg, X, y, cv=10, scoring="accuracy").mean()
+
+
+####GridSearchCV
+from sklearn.model_selection import GridSearchCV
+
+k_range = range(1,31)
+print k_range
+
+param_grid = dict(n_neighbors=k_range)
+print param_grid
+grid = GridSearchCV(knn, param_grid, cv=10, scoring='accuracy')
+print grid.fit(X,y)
+print grid.cv_results_
+
+print grid.cv_results_.keys()
