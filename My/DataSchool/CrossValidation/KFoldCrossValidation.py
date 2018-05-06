906cda46d94ce134d028b0ff18c6f606d1383ee8
GilShay <gilshayu@gmail.com>
2018-05-06 23:40:46 +0300

    RandomizedSearchCV

--- My/DataSchool/CrossValidation/KFoldCrossValidation.py
+++ My/DataSchool/CrossValidation/KFoldCrossValidation.py
@@ -43,6 +43,65 @@ param_grid = dict(n_neighbors=k_range)
 print param_grid
 grid = GridSearchCV(knn, param_grid, cv=10, scoring='accuracy')
 print grid.fit(X,y)
-print grid.cv_results_
-
 print grid.cv_results_.keys()
+print grid.cv_results_['std_test_score']
+grid_mean_scores = grid.cv_results_['mean_test_score']
+print grid_mean_scores
+print grid.cv_results_['params']
+
+plt.plot(k_range, grid_mean_scores)
+plt.xlabel('Value of K for KNN')
+plt.ylabel('Cross-Validated Accuracy')
+plt.show()
+
+print grid.best_score_
+print grid.best_params_
+print grid.best_estimator_
+print grid.best_index_
+
+
+#searching multiple parameters simultaneously
+weight_options = ['uniform', 'distance']
+
+param_grid = dict(n_neighbors=k_range, weights=weight_options)
+print param_grid
+
+grid = GridSearchCV(knn, param_grid, cv=10, scoring='accuracy')
+print grid
+print grid.fit(X,y)
+print grid.cv_results_['std_test_score']
+grid_mean_scores = grid.cv_results_['mean_test_score']
+print grid_mean_scores
+print grid.cv_results_['params']
+print grid.best_score_
+print grid.best_params_
+print grid.best_estimator_
+print grid.best_index_
+
+knn = KNeighborsClassifier(n_neighbors=13, weights='uniform')
+print knn
+print knn.fit(X,y)
+print knn.predict([[3, 5, 4, 2]])
+print grid.predict([[3, 5, 4, 2]])
+
+from sklearn.model_selection import RandomizedSearchCV
+
+param_dist = dict(n_neighbors=k_range, weights=weight_options)
+
+rand = RandomizedSearchCV(knn, param_dist, cv=10, scoring='accuracy', n_iter=10, random_state=5)
+print rand.fit(X,y)
+print rand.cv_results_['std_test_score']
+grid_mean_scores = rand.cv_results_['mean_test_score']
+print grid_mean_scores
+print rand.cv_results_['params']
+print rand.best_score_
+print rand.best_params_
+print rand.best_estimator_
+print rand.best_index_
+
+best_score = []
+for _ in range(20):
+     rand = RandomizedSearchCV(knn, param_dist, cv=10, scoring='accuracy', n_iter=10)
+     rand.fit(X, y)
+     best_score.append(round(rand.best_score_, 3))
+print best_score
