f6c67b609684f938cea6f5d8da4a81bddd52653b
GilShay <gilshayu@gmail.com>
2018-05-14 23:00:08 +0300

    SGD

---
+++ My/ML2/Part4_TrainingModels/StochasticGradientDecent.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+X = 2 * np.random.rand(100, 1)
+y = 4 + 3 * X + np.random.randn(100, 1)
+X_b = np.c_[np.ones((100, 1)), X] # add x0 = 1 to each instance
+
+m = 100
+n_epochs = 50
+t0, t1 = 5, 50
+
+def learning_schedule(t):
+    return t0 / float(t+t1)
+
+theta = np.random.randn(2,1)
+
+for epoch in range(n_epochs):
+    for i in range(m):
+        random_index = np.random.randint(m)
+        xi = X_b[random_index:random_index+1]
+        yi = y[random_index:random_index+1]
+        gradients = 2 * (xi.T.dot(xi.dot(theta) - yi))
+        eta = learning_schedule(epoch * m + i)
+        theta = theta - eta * gradients
+
+print theta
+
+from sklearn.linear_model import SGDRegressor
+sgd_reg = SGDRegressor(n_iter=50, penalty=None, eta0=0.1)
+sgd_reg.fit(X, y.ravel())
+
+print sgd_reg.intercept_, sgd_reg.coef_
