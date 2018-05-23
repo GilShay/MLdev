443ac7cb48edeaad1c689635cef4445020584866
GilShay <gilshayu@gmail.com>
2018-05-23 23:41:56 +0300

    Regularization and Logistic regression

--- My/ML2/Part4_TrainingModels/RegularizedLinearModels.py
+++ My/ML2/Part4_TrainingModels/RegularizedLinearModels.py
@@ -17,4 +17,27 @@ sgd_reg = SGDRegressor(penalty="l2")
 sgd_reg.fit(X, y.ravel())
 print sgd_reg.predict([[1.5]])
 
+
 ##Lasso Regression
+from  sklearn.linear_model import Lasso
+lasso_reg = Lasso(alpha=0.1)
+lasso_reg.fit(X, y)
+print lasso_reg.predict([[1.5]])
+
+##Elastic Net
+from sklearn.linear_model import ElasticNet
+elastic_net = ElasticNet(alpha=0.1, l1_ratio=0.5)
+elastic_net.fit(X,y)
+print elastic_net.predict([[1.5]])
+
+##Early Stopping
+# from sklearn.base import clone
+# from sklearn.model_selection import train_test_split
+# X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2)
+# sgd_reg = SGDRegressor(n_iter=1, warm_start=True, penalty=None, learning_rate="constant",eta0=0.0005)
+#
+# minimum_val_error = float("inf")
+# best_epoch = None
+# best_model = None
+# for  epoch in range(1000):
+#     sgd_reg.fit(X_train_poly_scaled, y_train)
