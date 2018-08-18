01be9cf23ae7b37115ac3ba06c59e30105058c7f
GilShay <gilshayu@gmail.com>
2018-08-18 21:37:19 +0300

    update TF

--- TensorFlow/part1_2_9/TensorFlow.py
+++ TensorFlow/part1_2_9/TensorFlow.py
@@ -22,3 +22,28 @@ init = tf.global_variables_initializer()
 with tf.Session() as sess:
     init.run()
     result = f.eval()
+
+x1 = tf.Variable(1)
+print(x1.graph is tf.get_default_graph())
+
+graph = tf.Graph()
+with graph.as_default():
+    x2 = tf.Variable(2)
+
+print(x2.graph is graph)
+print(x2.graph is tf.get_default_graph())
+
+w = tf.constant(3)
+x = w + 2
+y = x + 5
+z = x * 3
+
+with tf.Session() as sess:
+    print(y.eval())
+    print(z.eval())
+
+with tf.Session() as sess:
+    y_val, z_val = sess.run([y, z])
+    print(y_val)
+    print(z_val)
+    
